04859f9c4020a4d9bd73c0821f552fa87aeda473
ryosuke takasawa <ten.r0619@gmail.com>
2020-12-28 05:11:24 +0900

    abc069 bまで実施

--- abc069/a/main.rb
+++ abc069/a/main.rb
@@ -1,1 +1,3 @@
-input = gets.chomp.split("")
+side,vertical = gets.chomp.split(" ").map(&:to_i);
+block =  (side - 1) * (vertical - 1)
+puts block
--- abc069/b/main.rb
+++ abc069/b/main.rb
@@ -1,1 +1,5 @@
-input = gets.chomp.split("")
+input_word = gets.chomp.split(//)
+
+word_size = (input_word.size - 2).to_s
+
+puts input_word.first + word_size + input_word.last
