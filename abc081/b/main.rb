bbe64d710b7c5a88280e98dde573fce865d2ede1
rtakasawa <ten.r0619@gmail.com>
2022-12-18 23:37:17 +0900

    add abc081/b

--- abc081/b/main.rb
+++ abc081/b/main.rb
@@ -1,2 +1,15 @@
-input = gets.chomp.split("")
+def even_check(numbers)
+  even_count = 0
 
+  loop do
+    result = numbers.all? {|num| num.even? }
+    return even_count unless result
+
+    even_count += 1
+    numbers.map! {|num| num.div(2)}
+  end
+end
+
+n = gets.chomp.to_i
+numbers = gets.split(' ').map(&:to_i)
+puts even_check(numbers)
