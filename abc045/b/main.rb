495a929f0363fb164e393d506b7a06a28fc9683f
rtakasawa <ten.r0619@gmail.com>
2022-12-29 07:32:53 +0900

    add abc045/b

--- abc045/b/main.rb
+++ abc045/b/main.rb
@@ -1,1 +1,31 @@
-input = gets.chomp.split("")
+a = gets.chomp.split("")
+b = gets.chomp.split("")
+c = gets.chomp.split("")
+
+top_string = [a[0], b[0], c[0]].sort[0]
+result = nil
+
+loop do
+  case top_string
+  when 'a'
+    if a.dig(0)
+      top_string = a.shift
+    else
+      break result = 'A'
+    end
+  when 'b'
+    if b.dig(0)
+      top_string = b.shift
+    else
+      break result = 'B'
+    end
+  when 'c'
+    if c.dig(0)
+      top_string = c.shift
+    else
+      break result = 'C'
+    end
+  end
+end
+
+puts result
