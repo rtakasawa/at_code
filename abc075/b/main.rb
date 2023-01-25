26f23c89011aed0bf9cc4eeb716bbbf54e9fa930
ryosuke takasawa <r.takasawa@wiz-net.jp>
2023-01-26 07:11:53 +0900

    add abc075/b

--- abc075/b/main.rb
+++ abc075/b/main.rb
@@ -1,1 +1,34 @@
-input = gets.chomp.split("")
+h,w = gets.split(" ").map(&:to_i)
+ary = h.times.map{gets.split.map(&:to_s)}
+
+count = 0
+
+ary.each_with_index do |numbers, h_i|
+  numbers.each_with_index do |num, t_i|
+    count += 1
+    next if num == '#'
+
+    around = []
+
+    up = []
+    if h_i > 0
+      up.push(ary.dig(h_i - 1, count - w - 1)) if count > 0
+      up.push(ary.dig(h_i - 1, count - w + 1), ary.dig(h_i - 1, count - w))
+    end
+
+    down = [
+      ary.dig(h_i + 1, count - w - 1),
+      ary.dig(h_i + 1, count - w + 1),
+      ary.dig(h_i + 1, count - w),
+    ]
+
+    left = numbers.dig(t_i - 1)
+    right = numbers.dig(t_i + 1)
+
+    around.push(left,right).concat(up).concat(down)
+
+    ary[h_i][t_i] = around.count('#')
+  end
+end
+
+ary.each {|a| puts a}
