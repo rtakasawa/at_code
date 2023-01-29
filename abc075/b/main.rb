7683cb1a5c7dbb7da0579127befc308efbb75d98
rtakasawa <ten.r0619@gmail.com>
2023-01-30 07:23:54 +0900

    complete abc075/b

--- abc075/b/main.rb
+++ abc075/b/main.rb
@@ -1,34 +1,42 @@
-h,w = gets.split(" ").map(&:to_i)
-ary = h.times.map{gets.split.map(&:to_s)}
+h,_w = gets.split(" ").map(&:to_i)
+ary = h.times.map{gets.chomp.split('').map(&:to_s)}
 
-count = 0
+result = []
 
-ary.each_with_index do |numbers, h_i|
-  numbers.each_with_index do |num, t_i|
-    count += 1
-    next if num == '#'
+ary.each_with_index do |strings, h_i|
+  row_result = []
 
+  strings.each_with_index do |str, t_i|
+    next row_result.push(str) if str == '#'
+
+    # 周りの文字列を入れる
     around = []
 
+    # 上段
     up = []
     if h_i > 0
-      up.push(ary.dig(h_i - 1, count - w - 1)) if count > 0
-      up.push(ary.dig(h_i - 1, count - w + 1), ary.dig(h_i - 1, count - w))
+      up.push(ary.dig(h_i - 1, t_i - 1)) if t_i > 0
+      up.push(ary.dig(h_i - 1, t_i), ary.dig(h_i - 1, t_i + 1))
     end
 
-    down = [
-      ary.dig(h_i + 1, count - w - 1),
-      ary.dig(h_i + 1, count - w + 1),
-      ary.dig(h_i + 1, count - w),
-    ]
+    # 下段
+    down = []
+    down.push(ary.dig(h_i + 1, t_i - 1)) if t_i > 0
+    down.push(ary.dig(h_i + 1, t_i), ary.dig(h_i + 1, t_i + 1))
 
-    left = numbers.dig(t_i - 1)
-    right = numbers.dig(t_i + 1)
+    # 左右
+    left = strings.dig(t_i - 1) if t_i > 0
+    right = strings.dig(t_i + 1)
 
+    # 周りの文字をまとめる
     around.push(left,right).concat(up).concat(down)
 
-    ary[h_i][t_i] = around.count('#')
+    # [#]の数を数える
+    row_result.push(around.count('#'))
   end
+
+  # 行ごとに結果に入れる
+  result.push(row_result)
 end
 
-ary.each {|a| puts a}
+result.each {|row| puts row.join}
